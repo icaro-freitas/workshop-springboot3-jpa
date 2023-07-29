439f07fcc0f17dc2a0ecf49e541d8335177cd48a
icaro-freitas <icaro_freitas2014@outlook.com>
2023-07-29 16:09:11 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
